48825642db05be77f1d35492eb865d976693ce6e
Paulo Heleno <helenop16@gmail.com>
2025-09-14 00:33:37 -0300

    rota get adicionada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-console.log("Testandoooo");
+const express = require("express");
+const app = express();
+const port = 8080;
 
+let filmes = [
+  { id: 1, titulo: "O Senhor dos Anéis", ano: 2001 },
+  { id: 2, titulo: "Matrix", ano: 1999 },
+];
 
+app.get("/api/filmes", (req, res) => {
+  res.json(filmes);
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${port}`);
+});
